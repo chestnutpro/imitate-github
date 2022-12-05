a4ebf62e64e82fdaea40a4f0d50b36f252c627f2
chestnut <bw061188@gmail.com>
2022-12-05 18:59:29 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,6 +16,7 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
+  <n-button>naive-ui</n-button>
 
   <p>
     Check out
@@ -30,6 +31,14 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
+<script lang="ts">
+import { NButton } from 'naive-ui'
+export default {
+  components: {
+    NButton
+  }
+}
+</script>
 
 <style scoped>
 .read-the-docs {
